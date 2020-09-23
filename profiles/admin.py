5fe456447abd127457dcf54b759114c030ef0756
udit5656 <uditgarg5656@gmail.com>
2020-09-24 02:07:14 +0530

    Minor Changes

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Profile, FriendRequest
+from .models import Profile, FriendRequest, Message
 
 # Register your models here.
 admin.site.register(Profile)
 admin.site.register(FriendRequest)
+admin.site.register(Message)
