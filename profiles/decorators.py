b1a974e1713f6a126f4d239d2897382226944dcf
udit5656 <uditgarg5656@gmail.com>
2020-09-25 00:21:33 +0530

    Fixed bug in user_accept_friend_request decorator

--- profiles/decorators.py
+++ profiles/decorators.py
@@ -1,11 +1,11 @@
 from django.core.exceptions import PermissionDenied
-from .models import Profile,FriendRequest
+from .models import Profile, FriendRequest
 
 
 def user_can_accept_friend_request(function):
     def wrap(request, *args, **kwargs):
         friend_request = FriendRequest.objects.get(pk=kwargs['friend_request_id'])
-        if request.user.id != friend_request.request_to:
+        if request.user != friend_request.request_to:
             raise PermissionDenied
         else:
             return function(request, *args, **kwargs)
