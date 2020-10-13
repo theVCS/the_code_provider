abd762884b3e9a4b93baad2cf07f2159145f9770
udit5656 <uditgarg5656@gmail.com>
2020-10-13 23:17:58 +0530

    Fixed Minor issues in user search option

--- profiles/views.py
+++ profiles/views.py
@@ -9,7 +9,7 @@ from django.http import HttpResponse
 from .forms import UserSearchForm
 from .models import Profile, FriendRequest, Message
 from .decorators import user_can_accept_friend_request
-
+from django.contrib import messages
 Code = apps.get_model('editor', 'Code')
 
 
@@ -24,7 +24,7 @@ def profile_view(request, username):
     public_codes = Code.objects.filter(user=user, sharing_option='public').order_by('-date')
     private_codes = Code.objects.filter(user=user, sharing_option='private').order_by('-date')
     me_codes = Code.objects.filter(user=user, sharing_option='me').order_by('-date')
-    messages = Message.objects.filter(recieved_by=user)
+    messages1 = Message.objects.filter(recieved_by=user)
     pending_friend_requests = FriendRequest.objects.filter(status='pending', request_to=user)
 
     if request.method == 'POST':
@@ -32,17 +32,17 @@ def profile_view(request, username):
         if user_search_form.is_valid():
             try:
                 user = User.objects.get(username=user_search_form.cleaned_data['username'])
-                context = {'username': user}
+                context = {'username': user.username}
                 return HttpResponseRedirect(reverse('profiles:profile', kwargs=context))
             except User.DoesNotExist:
-                user_search_form.add_error('username', ValidationError("User Doesn't Exist"))
+                messages.add_message(request, messages.INFO, 'User not found')
     else:
         user_search_form = UserSearchForm()
 
     context = {'profile': profile, 'profile_user': user, 'title': 'Profile', 'friends_list': friends_list,
                'pending_friend_requests': pending_friend_requests, 'friends_count': friends_list.count(),
                'followers_list': followers_list, 'public_codes': public_codes, 'private_codes': private_codes,
-               'me_codes': me_codes, 'messages': messages, "user_search_form": user_search_form}
+               'me_codes': me_codes, 'messages1': messages1, "user_search_form": user_search_form}
     return render(request, 'profiles/profile.html', context)
 
 
